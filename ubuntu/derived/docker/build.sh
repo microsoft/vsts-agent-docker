66d72ad10946e12034967c9a524cbe6528f78786
Stephen Provine <stephpr@microsoft.com>
2016-10-19 16:20:16 -0700

    Move the derived docker directory.

--- ubuntu/derived/docker/build.sh
+++ ubuntu/derived/docker/build.sh
@@ -13,4 +13,6 @@ cd $(dirname $0)
 
 while read DOCKER_VERSION na; do
   docker build -t microsoft/vsts-agent:${VSTS_AGENT_TAG}docker-$DOCKER_VERSION $BASE_DIR/docker/$DOCKER_VERSION
+  ../standard/build.sh ${VSTS_AGENT_TAG}docker-$DOCKER_VERSION "$BASE_DIR/docker/$DOCKER_VERSION"
+
 done < <(cat versions | sed 's/\r//')
--- ubuntu/process.sh
+++ ubuntu/process.sh
@@ -5,10 +5,10 @@ PROCESS=$1
 
 while read UBUNTU_VERSION LIBICU_VERSION; do
   $PROCESS $UBUNTU_VERSION $LIBICU_VERSION
-  ../derived/docker/$PROCESS.sh ubuntu-$UBUNTU_VERSION "$(pwd)/$UBUNTU_VERSION"
+  derived/docker/$PROCESS.sh ubuntu-$UBUNTU_VERSION "$(pwd)/$UBUNTU_VERSION"
   VSTS_AGENT_VERSIONS="$(cat versioned/versions | grep -v ^# | sed 's/\r//')"
   for VSTS_AGENT_VERSION in $VSTS_AGENT_VERSIONS; do
     $PROCESS $UBUNTU_VERSION $LIBICU_VERSION $VSTS_AGENT_VERSION
-    ../derived/docker/$PROCESS.sh ubuntu-$UBUNTU_VERSION-$VSTS_AGENT_VERSION "$(pwd)/$UBUNTU_VERSION/$VSTS_AGENT_VERSION"
+    derived/docker/$PROCESS.sh ubuntu-$UBUNTU_VERSION-$VSTS_AGENT_VERSION "$(pwd)/$UBUNTU_VERSION/$VSTS_AGENT_VERSION"
   done
 done < <(cat versions | sed 's/\r//')
