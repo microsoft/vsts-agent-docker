161878634a59a95735555fcf6ba91f426be005b9
Adam Weigert <smokedlinq@outlook.com>
2017-08-03 08:42:07 -0400

    Update Start.ps1

--- windows/servercore/10.0.14393/Start.ps1
+++ windows/servercore/10.0.14393/Start.ps1
@@ -9,7 +9,7 @@ if ($env:VSTS_TOKEN -eq $null) {
     Write-Error "Missing VSTS_TOKEN environment variable"
     exit 1
 } else {
-    if (Test-Path -Path $env:VSTS_TOKEN -ItemType Leaf) {
+    if (Test-Path -Path $env:VSTS_TOKEN -PathType Leaf) {
         $env:VSTS_TOKEN = Get-Content -Path $env:VSTS_TOKEN -ErrorAction Stop | Where-Object {$_} | Select-Object -First 1
         
         if ([string]::IsNullOrEmpty($env:VSTS_TOKEN)) {
