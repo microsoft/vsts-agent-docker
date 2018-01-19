fa6a5f9daf2d3324c94636fa154afa680707de52
Ting <tihuang@microsoft.com>
2018-01-19 11:42:09 -0500

    consume netcore 2.x linux agent. (#59)

--- ubuntu/14.04/start.sh
+++ ubuntu/14.04/start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-UBUNTU_VERSION=$1
-
 export VSO_AGENT_IGNORE=_,MAIL,OLDPWD,PATH,PWD,VSTS_AGENT,VSTS_ACCOUNT,VSTS_TOKEN_FILE,VSTS_TOKEN,VSTS_POOL,VSTS_WORK,VSO_AGENT_IGNORE
 if [ -n "$VSTS_AGENT_IGNORE" ]; then
   export VSO_AGENT_IGNORE=$VSO_AGENT_IGNORE,VSTS_AGENT_IGNORE,$VSTS_AGENT_IGNORE
@@ -59,7 +57,7 @@ echo Determining matching VSTS agent...
 VSTS_AGENT_URL=$(curl -LsS \
   -u user:$(cat "$VSTS_TOKEN_FILE") \
   -H 'Accept:application/json;api-version=3.0-preview' \
-  "https://$VSTS_ACCOUNT.visualstudio.com/_apis/distributedtask/packages/agent?platform=ubuntu.$UBUNTU_VERSION-x64" \
+  "https://$VSTS_ACCOUNT.visualstudio.com/_apis/distributedtask/packages/agent?platform=linux-x64" \
   | jq -r '.value | map([.version.major,.version.minor,.version.patch,.downloadUrl]) | sort | .[length-1] | .[3]')
 if [ -z "$VSTS_AGENT_URL" -o "$VSTS_AGENT_URL" == "null" ]; then
   echo 1>&2 error: could not determine a matching VSTS agent
