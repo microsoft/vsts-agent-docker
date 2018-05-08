79c4282b6e018cadffa22b1fcc656f4f3566a1d8
Kirsten Kluge <kirsten.kluge@t-systems.com>
2018-05-08 16:36:00 +0200

    update build.ps1 to use more ram and change path

--- windows/build.ps1
+++ windows/build.ps1
@@ -5,10 +5,11 @@ Write-Output "`n------------------------------------------------------`n"
 foreach($dir in .\dirs.ps1){
     Write-Output "Build Docker Image for:`n    $dir"
     docker build --compress `
-    -t microsoft/vsts-agent:windows-$(($dir -replace '\\','-').ToLower() ) `
+    -t microsoft/vsts-agent:windows-$(($dir -replace '\\agent','' -replace '\\','-').ToLower() ) `
     --build-arg HTTP_PROXY="" `
     --build-arg HTTPS_PROXY="" `
     --build-arg NO_PROXY="" `
+    -m 6GB `
     ".\Output\$dir"
     Write-Output "`n------------------------------------------------------`n"
 }
